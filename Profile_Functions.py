6e1b69a5c75ef504b5fbac6602a31f7d1e2290ab
CDowey <39566243+CDowey@users.noreply.github.com>
2019-01-31 14:11:13 -0500

    Add files via upload
    Added to function documentation

--- Profile_Functions.py
+++ Profile_Functions.py
@@ -33,8 +33,22 @@ def feature_class_to_pandas_data_frame(feature_class, field_list):
 def Create_Profile(Line, DEM):
     """
     Creates a pandas DataFrame of the attribute table that results from the ArcGIS Stack Profile tool.
-    Useful for further analyzing or plotting profiles.
-    If the line input contains multiple lines these can be filtered by the LINE_ID field
+    Useful for further analyzing or plotting profiles in Python.
+    If the line input contains multiple lines these can be filtered by the LINE_ID field.
+    
+    Parameters
+    ----------
+    
+    Line : Vector
+        Lines(s) of cross-section
+        
+    DEM : Raster
+        Elevations are extracted from this Raster Digital Elevation Model to extract elevations
+        
+    Returns
+    ----------
+    
+    Pandas DataFrame object
     """
     arcpy.Delete_management('in_memory\profile')
     
@@ -47,21 +61,39 @@ def Create_Profile(Line, DEM):
     return df
 
 
-def Plot_Profile(Dataframe_x_z, line_color, xmax, xmin, ymax, ymin, aspect, shade_boolean):
+def Plot_Profile(profile_dataframe, line_color, xmax, xmin, ymax, ymin, aspect, shade):
     """
     Function that uses matplotlib to plot a simple graph for a profile
     line_color most easily supplied as HTML color code ie '#D0D0D0'
+    
+    Parameters
+    ----------
+    profile_dataframe : pandas DataFrame
+        A dataframe that contains distance and elevation information need to plot a profile.
+        Field names and format follow the DataFrame returned from the Create_Profile function.
+    line_color : string
+        HTML string for line color in plot (ex. '#D0D0D0')
+    xmax : int/float
+        Maximum value shown on x-axis. For visualization the xmax adds 5 to this value.
+    xmin : int/float
+        Minimum value shown on x-axis
+    ymax : int/float
+        Maximum value shown on y-axis
+    ymin : int/float
+        Minimum value shown on y-axis
+    aspect : int/float
+        Aspect ratio of plot
+    shade : boolean
+        True = Area beneath plot is shaded
     """
     fig = plt.figure()
     
-    plt.plot(Dataframe_x_z['FIRST_DIST'], Dataframe_x_z['FIRST_Z'], color = line_color)
+    plt.plot(profile_dataframe['FIRST_DIST'], profile_dataframe['FIRST_Z'], color = line_color)
     
     plt.xlabel('Distance (m)')
     plt.ylabel('Elevation (m)')
     
-#    plt.grid(True, color  = '#D0D0D0', linestyle = '--')
-    
-    plt.xlim(0, max(Dataframe_x_z['FIRST_DIST']) + 5)
+    plt.xlim(0, max(profile_dataframe['FIRST_DIST']) + 5)
     plt.ylim(ymin, ymax)
     plt.tight_layout(pad=0)
     
@@ -73,10 +105,9 @@ def Plot_Profile(Dataframe_x_z, line_color, xmax, xmin, ymax, ymin, aspect, shad
     # This is key to getting the x limits to work with a set aspect ratio!
     plt.gca().set_adjustable("box")
     
-    # Get rid of upper and right side bounding box
-    
-    if shade_boolean:
-        plt.gca().fill_between(Dataframe_x_z['FIRST_DIST'], Dataframe_x_z['FIRST_Z'], 0, facecolor= line_color, alpha = 0.1)
+    # If statement for shading beneath profile line
+    if shade:
+        plt.gca().fill_between(profile_dataframe['FIRST_DIST'], profile_dataframe['FIRST_Z'], 0, facecolor= line_color, alpha = 0.1)
     
     return fig
 
